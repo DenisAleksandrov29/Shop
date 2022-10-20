3db2b2f7e5fa128f25c4b00c648a18f19cdad89f
DenisAleksandrov29 <aie-dinis2007@yandex.ru>
2022-10-20 17:26:12 +0300

    Initial Commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Денис Александров on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
